f0f7def8760f514de1c8fdb840f82cd26e886802
Adi Kedmi <adi@masta.co.il>
2022-02-19 20:52:08 -0500

    Connect to the production version of the CMS

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "ah67pu4y",
-  dataset: "staging",
+  dataset: "production",
   apiVersion: "2021-10-21",
   token: "",
   useCdn: false,
