a9c60142fd892ca885c5479066ac9506603622a2
Adi Kedmi <adi@masta.co.il>
2022-02-19 21:47:29 -0500

    Utilize env variables to connect to sanity

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "ah67pu4y",
-  dataset: "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2021-10-21",
   token: "",
   useCdn: false,
