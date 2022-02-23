bf001c4716cb9a5a4e06265f82c33e04917343fc
Adi Kedmi <adi@masta.co.il>
2022-02-23 08:39:55 -0500

    fixup! fixup! Add gtag when user submits the contact form

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,25 @@ class MyDocument extends Document {
         <Head>
           {/*<!-- Google Tag Manager -->*/}
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA150272551-1"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-150272551-1');`,
+            }}
+          ></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-W5LLDJK');`,
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W5LLDJK');`,
             }}
           ></script>
           {/*<!-- End Google Tag Manager -->*/}
